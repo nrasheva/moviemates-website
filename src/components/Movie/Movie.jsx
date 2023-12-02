6013570686b97f143dd94bc5de70fd779cdd482c
nrasheva <nadezhda.rasheva96@gmail.com>
2023-12-02 19:48:01 +0200

    Minor UI improvements

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,6 +9,8 @@ import { setActiveMovie } from '../../redux/reducers/movies';
 import { Button } from '../Button/Button';
 
 export const Movie = (props) => {
+  const [opacity, setOpacity] = useState(0);
+
   const activeMovie = useSelector((state) => state.movies.activeMovie);
 
   const dispatch = useDispatch();
@@ -23,6 +25,8 @@ export const Movie = (props) => {
         if (entry.isIntersecting) {
           dispatch(setActiveMovie(props.movie));
         }
+
+        setOpacity(entry.isIntersecting ? 1 : 0);
       });
     };
 
@@ -46,7 +50,12 @@ export const Movie = (props) => {
   }, [dispatch, props.movie]);
 
   const Buttons = () => {
-    return <Button icon='' onClick={() => navigate(`/details/${activeMovie.id}`)} text='Details' type='filled' />;
+    return (
+      <>
+        <Button icon='' onClick={() => navigate(`/details/${activeMovie.id}`)} text='Details' type='filled' />
+        <Button icon='' onClick={() => {}} text='Genres' type='outlined' />
+      </>
+    );
   };
 
   return (
@@ -59,7 +68,7 @@ export const Movie = (props) => {
           props.movie.backdrop_path ? `https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}` : noise
         })`,
       }}>
-      <div className='hero-column'>
+      <div className='hero-column' style={{ opacity, transition: 'opacity 0.25s ease-in-out' }}>
         {/* <Genres handleActiveGenre={handleActiveGenre} /> */}
         <Content buttons={<Buttons />} heading={props.movie.title} subHeading={props.movie.overview} />
       </div>
