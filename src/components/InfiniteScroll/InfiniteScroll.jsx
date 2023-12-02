07692e44f8366ace499d3fa3af4536f58eba0588
nrasheva <nadezhda.rasheva96@gmail.com>
2023-12-02 14:29:39 +0200

    Update InfiniteScroll

--- src/components/InfiniteScroll/InfiniteScroll.jsx
+++ src/components/InfiniteScroll/InfiniteScroll.jsx
@@ -1,11 +1,19 @@
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import styles from './InfiniteScroll.module.css';
 import noise from '../../assets/noise.jpg';
 import { Content } from '../../components/Content/Content';
+import { Button } from '../Button/Button';
 
 export const InfiniteScroll = () => {
-  const movies = useSelector((state) => state.movies.movies);
+  const { activeMovie, movies } = useSelector((state) => state.movies);
+
+  const navigate = useNavigate();
+
+  const Buttons = () => {
+    return <Button icon='' onClick={() => navigate(`/details/${activeMovie.id}`)} text='Details' type='filled' />;
+  };
 
   return (
     <div className={styles['infinite-scroll']}>
@@ -21,7 +29,7 @@ export const InfiniteScroll = () => {
             }}>
             <div className='hero-column'>
               {/* <Genres handleActiveGenre={handleActiveGenre} /> */}
-              <Content buttons={<></>} heading={movie.title} subHeading={movie.overview} />
+              <Content buttons={<Buttons />} heading={movie.title} subHeading={movie.overview} />
             </div>
             <div className='hero-column' />
           </div>
